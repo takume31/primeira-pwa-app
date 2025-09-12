3d46c0e66afb9aac9374d6adcaa9335375f52114
takume31 <henrique.nakamura@estudante.ifms.edu.br>
2025-09-12 13:35:39 -0400

    ok

--- sw.js
+++ sw.js
@@ -1,19 +1,19 @@
-let cacheName ="my-first-pwa";
-let filesCache = [ "./", "./index.html",
-    "./CSS/style.css", "./js/main.js",];
+let cacheName = "my-first-pwas";
+let filesCache = ["/", "/index.html", "/CSS/style.css", "/js/main.js", "/manifest.json", "/html/exercicio1", "/html/exercicio2", "/html/exercicio3", "/html/exercicio5", "/html/exercicio6", "/html/exercicio7", "/html/exercicio8", "/html/exercicio9", "/html/exercicio10",
+   "/images/pwa-icon-256.png", "/images/pwa-icon-512.png"];
 
 self.addEventListener("install", (e) => {
-    e.waitUntil(
-        caches.open(cacheName).then(function(cache) {
-            return cache.addAll(filesCache);
-        })
-    );
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesCache);
+    })
+  );
 });
 
 self.addEventListener("fetch", (e) => {
-    e.respondWith(
-        caches.match(e.request).then((response) => {
-            return response || fetch(e.request);
-        })
-    );
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
